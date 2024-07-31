8463953aea5a77849ce5b85b7be41fe80e93aed5
PuneethKal <puneeth.kallem@gmail.com>
2024-07-31 13:11:56 -0400

    initial files

--- app/page.js
+++ app/page.js
@@ -1,95 +1,99 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { Box, Button, Modal, Stack, Typography} from "@mui/material";
+import { wrapApiHandler } from "next/dist/server/api-utils";
+import { firestore } from "@/firebase";
+import { collection, getDoc, addDoc, QuerySnapshot, query, onSnapshot } from "firebase/firestore";
+import { useEffect, useState  } from "react";
 
 export default function Home() {
+  const [items, setItems] = useState([]);
+
+  const [open,setOpen] = useState(false)
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  
+  //Add items to database
+  // const addItem = async (e) =>{
+  //   e.preventDefault()
+
+  //   await addDoc(collection(db, 'pantry')){
+  //      name: test_input
+  //   }
+    
+  // }
+
+  //Read Items from database
+    useEffect(() => {
+      const q = query(collection(firestore, 'pantry'))
+      const snapshots_query = onSnapshot(q, (querySnapshot) =>{
+        let itemsArr = []
+        querySnapshot.forEach((doc) => {
+          itemsArr.push(doc.id)
+        });
+        setItems(itemsArr)
+      })
+    },[])
+  //Delete items from database
+
+  // useEffect( () =>{
+  //   const updatePantry = async () => {
+  //     const snapshot  = query(collection(firestore,'pantry'))
+  //     const docs = await getDoc(snapshot)
+  //     docs.forEach((doc) => {
+  //       console.log(doc.id, doc.data())
+  //     })
+  //   }
+  //   updatePantry()
+  // },[])
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box
+      width="100vw"
+      height="100vh"
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      borderColor={"white"}
+      
+    >   
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Modal
+      open={open}
+      onClose={handleClose}
+      >hello
+      </Modal>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Button onClick={handleOpen}>Add!</Button>
+      <Box Box width="800px" border={"2px solid black"} borderRadius={"5px"}>
+      
+        <Box width="800px" height="100" bgcolor={"grey"} >
+          <Typography variant="h1" color="initial" textAlign={"center"}>Fruit</Typography>
+        </Box>
+        <Stack width='800px' height='200px' spacing={2} overflow={'auto'}>
+          {items.map((i) => (
+            <Box
+              key={i}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              height="300px"
+              width="100%"
+              bgcolor={"#D3D3D3"}
+              
+            >
+              <Typography variant="h6" color="initial">
+                {i.charAt(0).toLocaleUpperCase() + i.slice(1)}
+              </Typography>
+            </Box>
+          ))}
+        </Stack>
+      
+      </Box> 
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+    </Box>
+  
+    
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+  )
 }
---
+++ firebase.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from 'firebase/firestore'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4GJ8qbbOwTwo8QzZ-64ozmViYc_HeIPU",
+  authDomain: "pantry-db.firebaseapp.com",
+  projectId: "pantry-db",
+  storageBucket: "pantry-db.appspot.com",
+  messagingSenderId: "309369899237",
+  appId: "1:309369899237:web:9d1020880b4a50d513ac0c",
+  measurementId: "G-N7KPP7B42M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+export {app, firestore};
