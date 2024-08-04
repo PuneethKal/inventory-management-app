77a3c413b168dba1e139454048b4fa6d2877a645
puneeth <puneeth.kallem@gmail.com>
2024-08-04 18:47:07 -0400

    Adding login functionality

---
+++ app/Home/page.js
@@ -0,0 +1,207 @@
+"use client"
+import { Box, Button, Modal, Stack, Table, TextField, Typography, Grid, Paper, ButtonGroup} from "@mui/material";
+import { wrapApiHandler } from "next/dist/server/api-utils";
+import { firestore } from "@/firebase";
+import { collection, getDoc, addDoc, QuerySnapshot, query, onSnapshot, doc, setDoc, deleteDoc, where, documentId } from "firebase/firestore";
+import { useEffect, useState  } from "react";
+import { useRouter } from 'next/navigation';
+import { signOut,getAuth, onAuthStateChanged } from "firebase/auth";
+import { RequestCookiesAdapter } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+
+export default function Home() {
+
+  const color1 = "#171717"
+  const color2 = "#444444"
+  const color3 = "#DA0037"
+  const color4 = "#EDEDED"
+
+  const [items, setItems] = useState([]);
+  const router = useRouter()
+  const COLLECTIONS = "pantry"
+  const USERS = "users"
+  const [user, setUser] = useState(null);
+
+  //Add items to database
+  const addItem = async (item) =>{
+    // console.log(item)
+    await setDoc(doc(firestore, USERS, user.uid, COLLECTIONS, item), {Quantity:1})
+  }
+  //Increase Item Count
+  const IncreaseItemCount = async (item) =>{
+    await setDoc(doc(firestore, USERS, user.uid, COLLECTIONS, item.id), {Quantity:item.data().Quantity+1})
+    
+  }
+  //Decrease Item Count
+  const DecreaseItemCount = async (item) =>{
+    let newQuantitiy = item.data().Quantity-1
+    if (newQuantitiy == 0){
+      deleteItem(item.id)
+    }else{
+      await setDoc(doc(firestore, USERS, user.uid, COLLECTIONS, item.id), {Quantity:newQuantitiy})
+    }
+    
+  }
+
+  //Delete items from database
+  const deleteItem = async (item) => {
+    // console.log(item)
+    await deleteDoc(doc(firestore, USERS, user.uid, COLLECTIONS, item));
+  }
+
+  //Read Items from database
+  async function updateList(data="",c_user=user){
+    const q = query(collection(firestore, USERS, c_user.uid,COLLECTIONS))
+        const snapshots_query = onSnapshot(q, (querySnapshot) =>{
+          let itemsArr = []
+          querySnapshot.forEach((doc) => {
+            if ((doc.id.toLocaleLowerCase()).includes(data.toLocaleLowerCase())){
+              itemsArr.push(doc)
+            }
+          });
+          setItems(itemsArr)
+        })
+  }
+
+  useEffect(() => {
+    const authenicate = async () =>{
+      const auth = getAuth()
+      onAuthStateChanged(auth, (current_user) => {
+        setUser(current_user)
+        if (current_user) {
+          updateList("",current_user)
+        } else {
+          // User is signed out
+          router.push("/")
+        }
+      });
+    }
+    authenicate()
+  },[])
+  
+  async function logout(){
+      const auth = getAuth();
+      signOut(auth).then(() => {
+      // Sign-out successful.
+      console.log("Signout Success")
+      router.push("/")
+      }).catch((error) => {
+      // An error happened.
+        });
+    }
+
+  return (
+    <Box
+      width="100vw"
+      height="100vh"
+      display={"flex"}
+      alignItems={"center"}
+      flexDirection={"column"}
+      bgcolor={"#1B1B1B"}
+      gap={10}
+    >   
+      <Box 
+        width={"90%"}
+        maxHeight={"80px"}
+        minWidth={"800px"} 
+        padding={"20px"} 
+        display={"flex"}
+        alignItems={"center"}
+        margin={"40px"}
+        bgcolor={color4}
+        boxShadow={"3px 3px 2px 1px #d3d3d3 "}
+        borderRadius={"2px"}
+      >
+        <Button
+            onClick={(e)=>{
+                logout()
+            }}
+          >Sign out</Button>
+
+        <Typography variant="h3" color={color2} fontFamily={"Comic-Sans"} paddingLeft={"20%"}>
+          Inventory Manager
+        </Typography>
+      </Box>
+
+      {/* Inital colors were  bgcolor={"#0A0A0A"} border={"2px solid #252525"}*/}   
+      <Box bgcolor={"#1B1B1B"} border={"2px solid #1B1B1B"} paddingBottom={"10px"} borderRadius={"20px"} display={"flex"} >
+      {/* THIS IS THE TITLE FOR STACK */}
+      <Box Box width="800px">
+        <Box width="800px" height="100px" padding={"10px 15px 10px 10px"} display={"flex"} justifyContent={'center'} >
+          <Box 
+            width="100%"
+            display={"flex"} 
+            justifyContent={"flex-start"} 
+            padding={"0px 10px 0px 20px"}
+            bgcolor={color4}
+            boxShadow={"3px 3px 2px 1px #d3d3d3 "}
+            borderRadius={"2px"}
+          >
+            <Typography variant="h2" color={color2} flexGrow={1}>Inventory
+            </Typography>
+            
+            <Box width={"250px"} padding={"20px 10px"}>
+              <TextField 
+                label="Input / Search" 
+                variant="outlined" 
+                size="small"
+                onChange={(e) =>{ 
+                  let timeout = null
+                  timeout = setTimeout(function(){updateList(e.target.value)},500) }}
+                onKeyDown={(event) => {
+                  if (event.key === 'Enter') {
+                    addItem(event.target.value)
+                    event.target.value = ""
+                    updateList()
+                  }}}
+              />
+            </Box>
+            {/* <Button bgcolor={color3} >Insert</Button> */}
+          </Box>
+        </Box>
+        
+
+        {/* THIS IS THE STACK */}
+        <Stack width='800px' padding={"5px 0px"} height='200px' spacing={2} overflow={'auto'} alignItems={"center"}>
+          {items.map((i) => (
+          <Box 
+            container
+            key={i}
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            height="20%"
+            width="90%"
+            bgcolor={color4}
+            boxShadow={"3px 3px 2px 1px #d3d3d3 "}
+            borderRadius={"2px"}
+            
+          >
+            <Typography variant="h6" color={color2} flexGrow={1} paddingLeft={"30px"} >
+              {i.id.charAt(0).toLocaleUpperCase() + i.id.slice(1)}
+            </Typography>
+            <ButtonGroup size="small">
+              <Button onClick={ () => { DecreaseItemCount(i) }}>-</Button>
+              <Button>{i.data().Quantity}</Button>
+              <Button onClick={ () => { IncreaseItemCount(i) }} >+</Button>
+            </ButtonGroup>
+            
+            <Button
+              onClick={ () => {
+                deleteItem(i.id) 
+              }} 
+            >Trash</Button>
+            
+          </Box>
+            
+          ))}
+        </Stack>
+        </Box>
+      </Box> 
+
+    </Box>
+  
+    
+
+
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,168 +1,136 @@
 "use client"
-import { Box, Button, Modal, Stack, Table, TextField, Typography, Grid, Paper, ButtonGroup} from "@mui/material";
+import { Box, Button, Modal, Stack, Table, TextField, Typography, Grid, Paper, ButtonGroup } from "@mui/material";
 import { wrapApiHandler } from "next/dist/server/api-utils";
-import { firestore } from "@/firebase";
-import { collection, getDoc, addDoc, QuerySnapshot, query, onSnapshot, doc, setDoc, deleteDoc, where, documentId } from "firebase/firestore";
-import { useEffect, useState  } from "react";
-import { blue } from "@mui/material/colors";
-import { redirect } from 'next/navigation';
+import { app, firestore } from "@/firebase";
+import { useEffect, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { useRouter } from "next/navigation";
 
-export default function Home() {
+export default function Login() {
+    const color1 = "#171717"
+    const color2 = "#444444"
+    const color3 = "#DA0037"
+    const color4 = "#EDEDED"
 
-  const color1 = "#171717"
-  const color2 = "#444444"
-  const color3 = "#DA0037"
-  const color4 = "#EDEDED"
+    const [email, setEmail] = useState([])
+    const [password, setPassword] = useState([])
+    const router = useRouter()
+    const [user, setUser] = useState(null);
+    const [errorMsg, setErrorMsg] = useState([]);
 
-  const color11 = "#000000"
-  const color22 = "#F4DFC8"
-  const color33 = "#F4EAE0"
-  const color44 = "#FAF6F0"
+    const register = async (e) => {
+        // console.log(email, password)
+        const auth = getAuth(app);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                console.log("Success")
+                // Signed up 
+                setUser(userCredential.user)
+                console.log(user)
+                router.push('/Home')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(`Error [${errorCode}]: ${errorMessage}`);
+                console.log(error.code)
+                if (error.code == 'auth/invalid-email') {
+                    setErrorMsg("Invalid Email")
+                } else if (error.code == 'auth/weak-password') {
+                    setErrorMsg("Length of 6 characters required")
+                }
+                else if (error.code == 'auth/email-already-in-use') {
+                    setErrorMsg("Email already in use")
+                }
+                else if (error.code == 'auth/weak-password') {
+                    setErrorMsg("Length of 6 characters required")
+                } else {
 
-  const [items, setItems] = useState([]);
+                }
 
-  const COLLECTIONS = "one"
-  
-  
-  // const [collectionName, setCollectionName] = useState([]);
-  
-  //Add items to database
-  const addItem = async (item) =>{
-    // console.log(item)
-    await setDoc(doc(firestore, COLLECTIONS, item), {Quantity:1})
-  }
-  //Increase Item Count
-  const IncreaseItemCount = async (item) =>{
-    await setDoc(doc(firestore, COLLECTIONS, item.id), {Quantity:item.data().Quantity+1})
-    
-  }
-  //Decrease Item Count
-  const DecreaseItemCount = async (item) =>{
-    let newQuantitiy = item.data().Quantity-1
-    if (newQuantitiy == 0){
-      deleteItem(item.id)
-    }else{
-      await setDoc(doc(firestore, COLLECTIONS, item.id), {Quantity:newQuantitiy})
-    }
-    
-  }
+            });
 
-  //Read Items from database
-  function updateLlist(data=""){
-    const q = query(collection(firestore, COLLECTIONS))
-    const snapshots_query = onSnapshot(q, (querySnapshot) =>{
-      let itemsArr = []
-      querySnapshot.forEach((doc) => {
-        if ((doc.id.toLocaleLowerCase()).includes(data.toLocaleLowerCase())){
-          itemsArr.push(doc)
-        }
-      });
-      setItems(itemsArr)
-    })
-  }
-
-  useEffect(() => {
-    updateLlist()  
-  },[])
-  
-    
-
-  //Delete items from database
-    const deleteItem = async (item) => {
-      // console.log(item)
-      await deleteDoc(doc(firestore, COLLECTIONS, item));
     }
 
-  return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display={"flex"}
-      justifyContent={"center"}
-      alignItems={"center"}
-      flexDirection={"column"}
-      bgcolor={"#1B1B1B"}
-    >   
-        
-      <Box bgcolor={color1} paddingBottom={"10px"} borderRadius={"20px"} display={"flex"} >
-      {/* THIS IS THE TITLE FOR STACK */}
-      <Box Box width="800px">
-        <Box width="800px" height="100px" padding={"10px 15px 10px 10px"} display={"flex"} justifyContent={'center'} >
-          <Box 
-            width="100%"
-            display={"flex"} 
-            justifyContent={"flex-start"} 
-            padding={"0px 10px 0px 20px"}
-            bgcolor={color4}
-            boxShadow={"3px 3px 2px 1px #d3d3d3 "}
-            borderRadius={"2px"}
-          >
-            <Typography variant="h2" color={color2} flexGrow={1}>Fruit
-            </Typography>
-            
-            <Box width={"150px"} padding={"20px 10px"} bgcolor={blue}>
-              <TextField 
-                label="Input / Search" 
-                variant="outlined" 
-                size="small"
-                onChange={(e) =>{ 
-                  let timeout = null
-                  timeout = setTimeout(function(){updateLlist(e.target.value)},500) }}
-                onKeyDown={(event) => {
-                  if (event.key === 'Enter') {
-                    addItem(event.target.value)
-                    event.target.value = ""
-                    updateLlist()
-                  }}}
-              />
-            </Box>
-            <Button bgcolor={color3}>Insert</Button>
-          </Box>
-        </Box>
-        
+    const signIn = async (e) => {
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                setUser(userCredential.user)
+                console.log(user)
+                router.push('/Home')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+            });
+    }
 
-        {/* THIS IS THE STACK */}
-        <Stack width='800px' padding={"5px 0px"} height='200px' spacing={2} overflow={'auto'} alignItems={"center"}>
-          {items.map((i) => (
-          <Box 
-            container
-            key={i}
+    return (
+        <Box
+            width="100vw"
+            height="100vh"
             display={"flex"}
-            justifyContent={"center"}
             alignItems={"center"}
-            height="20%"
-            width="90%"
-            bgcolor={color4}
-            boxShadow={"3px 3px 2px 1px #d3d3d3 "}
-            borderRadius={"2px"}
-            
-          >
-            <Typography variant="h6" color={color2} flexGrow={1} paddingLeft={"30px"} >
-              {i.id.charAt(0).toLocaleUpperCase() + i.id.slice(1)}
-            </Typography>
-            <ButtonGroup size="small">
-              <Button onClick={ () => { DecreaseItemCount(i) }}>-</Button>
-              <Button>{i.data().Quantity}</Button>
-              <Button onClick={ () => { IncreaseItemCount(i) }} >+</Button>
-            </ButtonGroup>
-            
-            <Button
-              onClick={ () => {
-                deleteItem(i.id) 
-              }} 
-            >Trash</Button>
-            
-          </Box>
-            
-          ))}
-        </Stack>
+            bgcolor={"#1B1B1B"}
+            flexDirection={"column"}
+            gap={20}
+        >
+            <Box
+                width={"90%"}
+                maxHeight={"80px"}
+                minWidth={"800px"}
+                padding={"20px"}
+                display={"flex"}
+                alignItems={"center"}
+                margin={"40px"}
+                bgcolor={color4}
+                boxShadow={"3px 3px 2px 1px #d3d3d3 "}
+                borderRadius={"2px"}
+                justifyContent={"center``"}
+            >
+                <Typography variant="h3" color={color2} fontFamily={"Comic-Sans"}>
+                    Inventory Manager
+                </Typography>
+            </Box>
+            <Box
+                width="600px"
+                height="320px"
+                maxWidth={"60%"}
+                minWidth={"400px"}
+                display={"flex"}
+                padding={"20px"}
+                bgcolor={color4}
+                boxShadow={"3px 3px 2px 1px #d3d3d3 "}
+                borderRadius={"2px"}
+                gap={2}
+                flexDirection={"column"}
+            >
+                <Typography variant="h2" color={color2}>Login</Typography>
+                <Stack spacing={2} width={"80%"}>
+                    <TextField
+                        type="email"
+                        label={"Email"}
+                        onChange={(e) => {
+                            setEmail(e.target.value)
+                        }}
+                    ></TextField>
+                    <TextField
+                        label={"Password"}
+                        helperText={errorMsg}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                    ></TextField>
+                    <Box display={"flex"} flexDirection={"row"}>
+                        <Box flexGrow={1} onClick={(e) => { signIn(); setErrorMsg("") }}><Button>Login</Button></Box>
+                        <Button onClick={(e) => { register(); setErrorMsg("") }} >Register</Button>
+                    </Box>
+                </Stack>
+
+
+            </Box>
         </Box>
-      </Box> 
-
-    </Box>
-  
-    
-
-
-  )
+    )
 }
